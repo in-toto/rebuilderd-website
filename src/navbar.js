34f77b20eeb4ae314768995dbdcc5851e081c652
Aditya Sirish <aditya@saky.in>
2021-01-03 22:35:26 -0500

    Customize content for NYU SSL instance
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- src/navbar.js
+++ src/navbar.js
@@ -1,1 +1,16 @@
-../.external/archlinux-common-style/jsx/navbar.js
+import React from 'react';
+
+const ArchLinuxNavbar = () => (
+<div id="archnavbar">
+	<div id="logo"><a href="https://ssl.engineering.nyu.edu/" title="Return to the NYU SSL">Secure Systems Lab</a></div>
+	<div id="archnavbarmenu">
+		<ul id="archnavbarlist">
+			<li><a href="https://ssl.engineering.nyu.edu/" title="NYU SSL website">Home</a></li>
+			<li><a href="https://github.com/kpcyrd/rebuilderd" title="Rebuilderd">rebuilderd</a></li>
+			<li><a href="https://reproducible-builds.org" title="Reproducible Builds">Reproducible Builds</a></li>
+		</ul>
+	</div>
+</div>
+);
+
+export default ArchLinuxNavbar;
